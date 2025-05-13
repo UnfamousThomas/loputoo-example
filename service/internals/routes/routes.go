660f7b831dbc4407ff0c5089401260049a3b0df9
UnfamousThomas <thompalts@gmail.com>
2025-05-13 18:31:38 +0300

    Update route type

--- service/internals/routes/routes.go
+++ service/internals/routes/routes.go
@@ -10,7 +10,7 @@ import (
 // SetupRoutes sets up the nessecary routes, their handlers and starts serving http.
 func SetupRoutes(a *app.App) {
 	a.Mux.HandleFunc("POST /metrics", handlers.AddMetrics(a))
-	a.Mux.HandleFunc("GET /scale", handlers.ScaleGame(a))
+	a.Mux.HandleFunc("POST /scale", handlers.ScaleGame(a))
 	err := http.ListenAndServe(":8080", a.Mux)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
